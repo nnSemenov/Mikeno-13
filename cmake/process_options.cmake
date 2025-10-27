1166b4c9721ed468b7a2189d1fafa74e0e9c5c43
ToKiNoBug <tokinobug@163.com>
2025-10-27 14:44:43 +0800

    Add more wmake variables

--- cmake/process_options.cmake
+++ cmake/process_options.cmake
@@ -43,4 +43,46 @@ target_compile_options(OpenFOAM_Defines INTERFACE
 #    -Wno-shift-negative-value
 )
 
-set(LIB_SRC "${CMAKE_SOURCE_DIR}/src")
+####### wmake env vars
+set(WM_PROJECT_DIR ${PROJECT_SOURCE_DIR})
+set(WM_CC ${CMAKE_C_COMPILER})
+set(WM_CXX ${CMAKE_CXX_COMPILER})
+set(WM_PROJECT ${PROJECT_NAME})
+set(WM_COMPILER_LIB_ARCH ${CMAKE_SIZEOF_VOID_P})
+set(WM_LINK_LANGUAGE ${CMAKE_CXX_LINK_EXECUTABLE})
+
+if(${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
+    set(WM_COMPILER Gcc)
+elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+    set(WM_COMPILER Clang)
+elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL "Intel")
+    set(WM_COMPILER Icx)
+else ()
+    set(WM_COMPILER Unknown)
+    message(WARNING "Unknown C++ compiler id ${CMAKE_CXX_COMPILER_ID}, WM_COMPILER is set to ${CMAKE_CXX_COMPILER_ID}")
+endif ()
+
+if(${CMAKE_BUILD_TYPE} STREQUAL Debug)
+    set(WM_COMPILE_OPTION Debug)
+elseif (${CMAKE_BUILD_TYPE} STREQUAL Release)
+    set(WM_COMPILE_OPTION Opt)
+elseif (${CMAKE_BUILD_TYPE} STREQUAL MinSizeRel)
+    set(WM_COMPILE_OPTION Opt)
+elseif (${CMAKE_BUILD_TYPE} STREQUAL RelWithDebInfo)
+    set(WM_COMPILE_OPTION Prof)
+else ()
+    set(WM_COMPILE_OPTION Unknown)
+    message(WARNING "Unknown CMAKE_BUILD_TYPE ${CMAKE_BUILD_TYPE}, WM_COMPILE_OPTION is set to Unknown")
+endif ()
+
+set(WM_OPTIONS "${WM_ARCH}${WM_COMPILER}${WM_PRECISION_OPT}${WM_COMPILE_OPTION}")
+message(STATUS "WM_OPTIONS = ${WM_OPTIONS}")
+
+###### source env vars
+set(LIB_SRC "${WM_PROJECT_DIR}/src")
+
+
+##### Run env vars
+set(FOAM_APP ${WM_PROJECT_DIR}/applications)
+set(FOAM_SOLVERS ${FOAM_APP}/solvers)
+set(FOAM_SRC ${WM_PROJECT_DIR}/src)
