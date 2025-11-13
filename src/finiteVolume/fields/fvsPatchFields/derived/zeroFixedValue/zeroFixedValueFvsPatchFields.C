b49835a98246150cdc9be64da000c20e4d57c0a8
ToKiNoBug <tokinobug@163.com>
2025-11-14 00:04:12 +0800

    Fix finiteVolume (and multiphaseEuler

--- src/finiteVolume/fields/fvsPatchFields/derived/zeroFixedValue/zeroFixedValueFvsPatchFields.C
+++ src/finiteVolume/fields/fvsPatchFields/derived/zeroFixedValue/zeroFixedValueFvsPatchFields.C
@@ -2,11 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-<<<<<<<< HEAD:src/meshTools/meshSearch/meshSearchBoundBox.C
-    \\  /    A nd           | Copyright (C) 2011-2025 OpenFOAM Foundation
-========
     \\  /    A nd           | Copyright (C) 2025 OpenFOAM Foundation
->>>>>>>> upstream/master:src/finiteVolume/fields/fvsPatchFields/derived/zeroFixedValue/zeroFixedValueFvsPatchFields.C
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -27,48 +23,20 @@ License
 
 \*---------------------------------------------------------------------------*/
 
-<<<<<<<< HEAD:src/meshTools/meshSearch/meshSearchBoundBox.C
-#include "meshSearchBoundBox.H"
-========
 #include "zeroFixedValueFvsPatchFields.H"
 #include "surfaceFields.H"
 #include "addToRunTimeSelectionTable.H"
->>>>>>>> upstream/master:src/finiteVolume/fields/fvsPatchFields/derived/zeroFixedValue/zeroFixedValueFvsPatchFields.C
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
 namespace Foam
 {
-<<<<<<<< HEAD:src/meshTools/meshSearch/meshSearchBoundBox.C
-    defineTypeNameAndDebug(meshSearchBoundBox, 0);
-}
-========
->>>>>>>> upstream/master:src/finiteVolume/fields/fvsPatchFields/derived/zeroFixedValue/zeroFixedValueFvsPatchFields.C
 
 // * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
 
 makeFvsPatchFields(zeroFixedValue);
 
-<<<<<<<< HEAD:src/meshTools/meshSearch/meshSearchBoundBox.C
-Foam::meshSearchBoundBox::meshSearchBoundBox(const polyMesh& mesh)
-:
-    DemandDrivenMeshObject
-    <
-        polyMesh,
-        DeletableMeshObject,
-        meshSearchBoundBox
-    >(mesh),
-    bb_(treeBoundBox(mesh.points()).extend(1e-4))
-{}
-
-
-// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
-
-Foam::meshSearchBoundBox::~meshSearchBoundBox()
-{}
-========
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
->>>>>>>> upstream/master:src/finiteVolume/fields/fvsPatchFields/derived/zeroFixedValue/zeroFixedValueFvsPatchFields.C
 
 } // End namespace Foam
 
